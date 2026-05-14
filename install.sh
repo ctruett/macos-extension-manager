664b5cef1b8937b08cf720b6a5455063e3eb08eb
ctruett <ctruett@users.noreply.github.com>
2026-05-14 17:35:32 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+BINARY_NAME="system-extension-manager"
+INSTALL_DIR="/usr/local/bin"
+
+echo "Building $BINARY_NAME..."
+cargo build --release
+
+echo "Installing to $INSTALL_DIR/$BINARY_NAME..."
+sudo cp "target/release/$BINARY_NAME" "$INSTALL_DIR/$BINARY_NAME"
+sudo chmod +x "$INSTALL_DIR/$BINARY_NAME"
+
+echo "Installed: $(which $BINARY_NAME)"
